afd2b0c207210e2ec23afeda8128b1f3d4315a3f
murkomenjustine <justinemukomen@gmail.com>
2020-03-05 19:51:43 +0300

    Update setting_script.php

--- setting_script.php
+++ setting_script.php
@@ -2,7 +2,7 @@
     session_start();
     require 'connection.php';
     if(!isset($_SESSION['email'])){
-        header('location:index.php');
+        header('location:index.html');
     }  
     $old_password= md5(md5(mysqli_real_escape_string($con,$_POST['oldPassword'])));
     $new_password= md5(md5(mysqli_real_escape_string($con,$_POST['newPassword'])));
